7e8d12ef257cae98460489a061022a9f5c7521f3
ToastedToast <hey@toastify.tk>
2023-09-28 19:32:57 +0800

    export more in discordic

--- packages/discordic/src/index.ts
+++ packages/discordic/src/index.ts
@@ -1,3 +1,14 @@
-export * from "./client";
+export * from "./models/client-user";
+export * from "./models/guild";
+export * from "./models/message";
+export * from "./models/user";
+
+export * from "./types/channel";
+export * from "./types/client";
 export * from "./types/embed";
+export * from "./types/guild";
+export * from "./types/images";
 export * from "./types/intents";
+export * from "./types/presence";
+
+export * from "./client";
