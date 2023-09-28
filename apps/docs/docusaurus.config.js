78fa6808c0dffe82c093e5c823bcefcd345733ba
ToastedToast <hey@toastify.tk>
2023-09-28 18:56:31 +0800

    update theme

--- apps/docs/docusaurus.config.js
+++ apps/docs/docusaurus.config.js
@@ -63,6 +63,10 @@ const config = {
     ({
       // Replace with your project's social card
       image: "img/docusaurus-social-card.jpg",
+      colorMode: {
+        defaultMode: "dark",
+        respectPrefersColorScheme: true,
+      },
       navbar: {
         title: "My Site",
         logo: {
