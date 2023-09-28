ca5ef8e260fc0654d438562caee190aef5fc3c36
ToastedToast <hey@toastify.tk>
2023-09-28 18:37:20 +0800

    move to github pages

--- apps/docs/docusaurus.config.js
+++ apps/docs/docusaurus.config.js
@@ -12,15 +12,16 @@ const config = {
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://discordic.js.org",
+  url: "https://toasteddev.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/",
+  baseUrl: "/discordic/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: "ToastedDev", // Usually your GitHub org/user name.
   projectName: "discordic", // Usually your repo name.
+  trailingSlash: false,
 
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
