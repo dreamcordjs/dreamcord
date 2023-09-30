7eec22dcef0bcc570bbd967b914c560fd60589a5
ToastedToast <hey@toastify.tk>
2023-09-30 18:08:08 +0800

    better docs design, use new logo

--- apps/docs/src/pages/index.js
+++ apps/docs/src/pages/index.js
@@ -13,13 +13,8 @@ function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
   return (
     <header className={clsx("hero hero--primary", styles.heroBanner)}>
-      <div className="container">
-        <img
-          src="/dreamcord/img/logo-inverted.png"
-          alt="Dreamcord Logo"
-          width={250}
-          height={190}
-        />
+      <div className="container" style={{ zIndex: "0" }}>
+        <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
           <Link
