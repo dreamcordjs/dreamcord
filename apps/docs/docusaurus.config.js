d44fd5afae7f6b647ff19639f879a96d072adad7
ToastedToast <hey@toastify.tk>
2023-10-01 06:30:35 +0800

    update url

--- apps/docs/docusaurus.config.js
+++ apps/docs/docusaurus.config.js
@@ -12,10 +12,10 @@ const config = {
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://dreamcordjs.github.io",
+  url: "https://dreamcord.js.org",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/dreamcord/",
+  baseUrl: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
